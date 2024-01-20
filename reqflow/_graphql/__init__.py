9553091f054e3826a537b6a76f40277cf8685042
Olxii P <>
2024-01-20 17:09:35 +0200

    Remove graphql from the main branch

--- reqflow/_graphql/__init__.py
+++
@@ -1,4 +0,0 @@
-from .client import ClientGraph
-from .request import GraphQLRequest
-from .fluent_api_graphql import GivenGraphQL, WhenGraphQL, ThenGraphQL
-from .fluent_api_graphql import given_graphql
