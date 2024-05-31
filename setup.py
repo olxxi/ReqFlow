c1429489248ed56ba74068e4830710176e0e0f40
olxii <piskunovalxy@gmail.com>
2024-05-31 17:33:41 +0300

    bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ReqFlow',
-    version='1.0.4-alpha',
+    version='1.0.5-alpha',
     packages=find_packages(),
     install_requires=[
         'httpx>=0.26.0',
