8f6bc190c51aa5e9b2d0439953767f822be9825a
olxii <piskunovalxy@gmail.com>
2024-06-30 21:12:00 +0300

    Bump up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ReqFlow',
-    version='1.0.7-alpha',
+    version='1.0.8-alpha',
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=[
